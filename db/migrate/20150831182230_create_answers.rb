60c85f58b2649c595c4ed80b282cb4bb0683cff9
Rachelle Tanase <rachelle.tanase@yahoo.com>
2015-08-31 15:15:47 -0700

    Created database seeds

--- db/migrate/20150831182230_create_answers.rb
+++ db/migrate/20150831182230_create_answers.rb
@@ -2,8 +2,8 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
         t.string :option
-        t.string :value
         t.belongs_to :question, index: true
+        t.belongs_to :survey, index:true
     end
   end
 end
