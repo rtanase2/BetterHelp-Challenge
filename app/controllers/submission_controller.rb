af97155af1c12d7f989807b29d22105402ad6a22
Rachelle Tanase <rachelle.tanase@yahoo.com>
2015-09-01 13:22:58 -0700

    Submissions successfully record
    Working on displaying the statistics

--- app/controllers/submission_controller.rb
+++ app/controllers/submission_controller.rb
@@ -1,28 +1,26 @@
 class SubmissionController < ApplicationController
+   def index
+        @survey = Survey.find(params[:survey_id])
+   end
+
     def new
         @submission = Submission.new
         @survey = Survey.find(params[:survey_id])
         @questions = Question.where(:survey_id => params[:survey_id])
         @answers = Answer.where(:survey_id => params[:survey_id])
-        @submission.results = {}
     end
 
     def create
         @submission = Submission.new(submission_params)
         @submission.survey_id = params[:survey_id]
         @questions = Question.where(:survey_id => params[:survey_id])
+        @submission.results = {}
         for q in @questions
             @field = params[:submission][:results][q.id.to_s]
-            if  @field.kind_of?(Array)
-                @field.count.times do |i|
-                    print @field[i]
-                end
-            else
-                print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!" <<  @field << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 
-            end
+            @submission.results[q.id.to_s] = @field
         end
         if @submission.save
-            redirect_to '/survey/' << params[:survey_id]
+            redirect_to '/survey/' << params[:survey_id] << "/submission"
         else
             render 'new'
         end
