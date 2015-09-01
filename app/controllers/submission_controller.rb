26e9a6118c1d1ba895522e968f40477bb4c73bd3
Rachelle Tanase <rachelle.tanase@yahoo.com>
2015-09-01 12:21:19 -0700

    Separates questions by input

--- app/controllers/submission_controller.rb
+++ app/controllers/submission_controller.rb
@@ -4,13 +4,25 @@ class SubmissionController < ApplicationController
         @survey = Survey.find(params[:survey_id])
         @questions = Question.where(:survey_id => params[:survey_id])
         @answers = Answer.where(:survey_id => params[:survey_id])
-        @submission.results ||= {} 
+        @submission.results = {}
     end
 
     def create
         @submission = Submission.new(submission_params)
+        @submission.survey_id = params[:survey_id]
+        @questions = Question.where(:survey_id => params[:survey_id])
+        for q in @questions
+            @field = params[:submission][:results][q.id.to_s]
+            if  @field.kind_of?(Array)
+                @field.count.times do |i|
+                    print @field[i]
+                end
+            else
+                print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!" <<  @field << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 
+            end
+        end
         if @submission.save
-            redirect_to ''
+            redirect_to '/survey/' << params[:survey_id]
         else
             render 'new'
         end
